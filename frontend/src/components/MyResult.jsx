85a9313ee5f12769e5744d4aca0c78a38717a139
MD Hasin Anjum <127542579+pillowarian@users.noreply.github.com>
2026-01-16 14:38:52 +0600

    Update MyResult.jsx

--- frontend/src/components/MyResult.jsx
+++ frontend/src/components/MyResult.jsx
@@ -14,7 +14,7 @@ const Badge = ({ percent }) => {
   return <span className={resultStyles.badgeNeedsWork}>Needs Work</span>;
 };
 
-const MyResult = ({ apiBase = "http://localhost:4000" }) => {
+const MyResult = ({ apiBase = "http://https://quizappnew-backend.onrender.com" }) => {
 const [results, setResults] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
